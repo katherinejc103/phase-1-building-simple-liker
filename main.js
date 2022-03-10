d36778fab235e92d3dcdb7993a759c294950d21b
Katherine Jean-Charles <katherinejc103@gmail.com>
2022-03-09 19:11:28 -0500

    Done

--- main.js
+++ main.js
@@ -1,10 +1,50 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector('#modal')
 
 // Your JavaScript code goes here!
+// function clickHeart(){
+//   mimicServerCall();
 
+// }
 
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM Content was Loaded')
+  errorModal.classList.add('hidden')
+  // findLikes();
+  clickListner();
+})
+
+// function findLikes() {
+//   const likeArr = document.querySelectorAll(".like-gyph")
+  
+//   likeArr.forEach((singularLike) => {
+//     singularLike.addEventListener("click", () => console.log("YOU FOUND ME "))
+//   })
+// }
+
+function clickListner(){
+  document.addEventListener('click', (event) => {
+    if(event.target.classList.value +++ 'like-glyph'){
+      console.log('YOU FOUND ME! LIKE!')
+    }
+  })
+}
+
+if(event.target.classList[0] === 'like-glyph'){
+  mimicServerCall()
+    .then((response) => {
+    })
+    .catch((error) => {
+    console.log(error);
+    errorModal.remove("hidden");
+    setTimeOut(() => {
+      hideError()
+    }, 3000)
+    })
+
+}
 
 
 //------------------------------------------------------------------------------
